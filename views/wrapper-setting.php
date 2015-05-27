be57766a7daa98552fa51edda990cde3c7ae87fb
CharlieC <charlie@idxbroker.com>
2015-05-27 18:34:04 +0000

    Release 1.1.7 for bug fix.

---
+++ views/wrapper-setting.php
@@ -0,0 +1,4 @@
+<ul>
+	<li><span>Your dynamic wrapper page link:</span> <a href="http://local.wordpress.dev/?p=6" taget="_blank">Wrapper Page</a></li>
+	<li><code>http://local.wordpress.dev/?p=6</code></li>
+</ul>
