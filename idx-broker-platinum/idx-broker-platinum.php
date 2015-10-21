be2c4727f094d92d6c4dfeef186d3885f23ca52e
sheparddw <daniel@idxbroker.com>
2015-10-21 12:24:14 -0700

    Fix merge conflicts and formatting issues with the readme file.

--- idx-broker-platinum/idx-broker-platinum.php
+++ idx-broker-platinum/idx-broker-platinum.php
@@ -26,7 +26,6 @@ class Idx_Broker_Plugin
         if ($this->php_version_check()) {
             require_once 'idx' . DIRECTORY_SEPARATOR . 'autoloader.php';
             new IDX\Initiate_Plugin();
-            include 'idx' . DIRECTORY_SEPARATOR . 'backwards-compatibility.php';
             /** Function that is executed when plugin is activated. **/
             register_activation_hook(__FILE__, array($this, 'idx_activate'));
             register_deactivation_hook(__FILE__, array($this, 'idx_deactivate'));
