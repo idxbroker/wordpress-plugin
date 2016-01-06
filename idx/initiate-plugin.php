41b94e8d9484f009d639a04d4bab9efe0ca7b0b3
Chris Sjoblom <chriss@idxbroker.com>
2016-01-06 14:09:18 -0800

    Fixed SVN commit errors that were pushing back the WP.org svn commits.

--- idx/initiate-plugin.php
+++ idx/initiate-plugin.php
@@ -183,8 +183,9 @@ class Initiate_Plugin
         add_menu_page('IMPress for IDX Broker Settings', 'IMPress', 'administrator', 'idx-broker', array($this, 'idx_broker_platinum_admin_page'), 'none', 55.572);
         add_submenu_page('idx-broker', 'IMPress for IDX Broker Plugin Options', 'Initial Settings', 'administrator', 'idx-broker', array($this, 'idx_broker_platinum_admin_page'));
         //Only add Omnibar page if no errors in API
-        if (!isset($this->idx_api->idx_api_get_systemlinks()->errors) &&
-            !empty($this->idx_api->idx_api_get_systemlinks())) {
+        $api_errors = $this->idx_api->idx_api_get_systemlinks()->errors;
+        $api_systemlinks = $this->idx_api->idx_api_get_systemlinks();
+        if (!isset($api_errors) && !empty($api_systemlinks)) {
             add_submenu_page('idx-broker', 'Omnibar Settings', 'Omnibar Settings', 'administrator', 'idx-omnibar-settings', array($this, 'idx_omnibar_settings_interface'));
         }
         add_action('admin_footer', array($this, 'add_upgrade_center_link'));
@@ -238,8 +239,10 @@ class Initiate_Plugin
             'href' => admin_url('admin.php?page=idx-omnibar-settings'),
         );
         //Only add Omnibar page if no errors in API
-        if (!isset($this->idx_api->idx_api_get_systemlinks()->errors) &&
-            !empty($this->idx_api->idx_api_get_systemlinks())) {
+        $api_errors = $this->idx_api->idx_api_get_systemlinks()->errors;
+        $api_systemlinks = $this->idx_api->idx_api_get_systemlinks();
+        if (!isset($api_errors) &&
+            !empty($api_systemlinks)) {
             $wp_admin_bar->add_node($args);
         }
         $args = array(
