147aa98a022de960f7b80fddb5553bc3277f272b
sheparddw <daniel@idxbroker.com>
2016-02-11 13:10:42 -0800

    Add X button to top right, hide notification immediately, and fix styling inconsistencies with common plugins.

--- idx/review-prompt.php
+++ idx/review-prompt.php
@@ -9,7 +9,7 @@ class Review_Prompt
         add_action('admin_init', array($this, 'check_timestamp'));
         add_action('wp_ajax_idx_dismiss_review_prompt', array($this, 'dismiss_prompt'));
         //remove dismiss option for testing
-        // delete_option('idx_dismiss_review_prompt');
+        delete_option('idx_dismiss_review_prompt');
     }
 
     public static function set_timestamp()
@@ -42,11 +42,11 @@ class Review_Prompt
         $review_url = 'https://wordpress.org/support/view/plugin-reviews/idx-broker-platinum';
         $dismiss_url = esc_url(wp_nonce_url(admin_url('?idx_dismiss_review_prompt=true'), 'idx_review_prompt_nonce'));
         
-        echo '<div class="updated"><p>';
+        echo '<div class="updated idx_review_prompt is-dismissible"><p>';
         echo 'Loving the updated IMPress for IDX Broker? If so, please leave us a review with your feedback!<br><br>';
         echo "<a href=\"$review_url\" class=\"button button-primary idx_accept_review_prompt\" target=\"_blank\">Review IMPress Now</a> ";
         echo "<a href=\"#\" class=\"idx_dismiss_review_prompt\">No Thanks</a>";
-        echo '</p></div>';
+        echo '</p><button type="button" class="notice-dismiss"><span class="screen-reader-text">Dismiss this notice.</span></button></div>';
         //dismiss via ajax
         wp_enqueue_script('idx_dismiss_review_prompt', plugins_url('/assets/js/idx-dismiss-review-prompt.min.js', dirname(__FILE__)), 'jquery');
 
