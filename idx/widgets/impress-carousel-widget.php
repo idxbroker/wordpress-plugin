c0f842b65078872f63761c29d36a200d840606f5
Dave Bonds <dave@idxbroker.com>
2017-01-19 08:01:02 -0800

    remove number_format due to some MLSs including commas

--- idx/widgets/impress-carousel-widget.php
+++ idx/widgets/impress-carousel-widget.php
@@ -199,7 +199,7 @@ class Impress_Carousel_Widget extends \WP_Widget
             $output .= $this->hide_empty_fields('beds', 'Beds', $prop['bedrooms']);
             $output .= $this->hide_empty_fields('baths', 'Baths', $prop['totalBaths']);
             //remove decimals and add commas to SqFt value
-            $output .= $this->hide_empty_fields('sqft', 'SqFt', number_format($prop['sqFt']));
+            $output .= $this->hide_empty_fields('sqft', 'SqFt', $prop['sqFt']);
             $output .= "</p>";
 
             //Add Disclaimer and Courtesy.
