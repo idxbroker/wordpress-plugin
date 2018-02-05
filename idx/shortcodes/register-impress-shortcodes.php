10d58bf13cfa7ea466246647c6d5ec656e40c173
Erik Brown <erikb@IDX.LOCAL>
2018-02-05 11:06:29 -0800

    Removed inline css

--- idx/shortcodes/register-impress-shortcodes.php
+++ idx/shortcodes/register-impress-shortcodes.php
@@ -39,21 +39,7 @@ class Register_Impress_Shortcodes
 
         // Returns hidden if false or not set
         $password_field_type = $password_field ? 'password' : 'hidden';
-
-        if($password_field_type === 'password') {
-
-            $password_exists_css = '
-                #impress-widgetPassword, #impress-widgetEmail {
-                    width: 30%;
-                }
-
-                #impress-widgetLeadLoginSubmit {
-                    width: 36%;
-                }
-            ';
-
-            wp_add_inline_style('impress-lead-login', $password_exists_css);
-        }
+        $password_label = $password_field ? '<label for=\'impress-widgetPassword\'>Password:</label>' : '';
 
         $widget = sprintf('
             <form action="%1$sajax/userlogin.php" class="impress-lead-login" method="post" target="%2$s" name="leadLoginForm">
@@ -61,9 +47,10 @@ class Register_Impress_Shortcodes
                 <input type="hidden" name="loginWidget" value="true">
                 <label for="impress-widgetEmail">Email Address:</label>
                 <input id="impress-widgetEmail" type="text" name="email" placeholder="Enter your email address">
-                <input id="impress-widgetPassword" type="%3$s" name="password" placeholder="Password">
+                %3$s
+                <input id="impress-widgetPassword" type="%4$s" name="password" placeholder="Password">
                 <input id="impress-widgetLeadLoginSubmit" type="submit" name="login" value="Log In">
-            </form>', $this->idx_api->subdomain_url(), $target, $password_field_type);
+            </form>', $this->idx_api->subdomain_url(), $target, $password_label, $password_field_type);
 
         return $widget;
     }
