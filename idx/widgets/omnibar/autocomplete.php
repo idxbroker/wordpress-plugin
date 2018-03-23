dbee8fced1416fcc551d43b1c598193b9521c1a0
Erik Brown <erikb@IDX.LOCAL>
2018-03-23 15:53:14 -0700

    REST endpoint now actually calls the database can returns the data in an array

--- idx/widgets/omnibar/autocomplete.php
+++ idx/widgets/omnibar/autocomplete.php
@@ -1,20 +1,31 @@
 <?php
 namespace IDX\Widgets\Omnibar;
 
-class Autocomplete
-{
-    public function __construct() {
+class Autocomplete {
+	public function __construct() {
 
-    }
+	}
 
-    public static function get_autocomplete_data($request) {
-        return [
-            'item1',
-            'item2',
-            'item3',
-            'item4',
-            'item5'
-        ];
-    }
+	public function get_autocomplete_data( $data ) {
+
+		$search_text = urldecode($data['query']);
+
+		global $wpdb;
+
+		$table_name = $wpdb->prefix . 'idxbroker_autocomplete_values';
+
+		// Direct db call is fine, since we can't cache every search variant
+		$results = $wpdb->get_results(
+			"SELECT value FROM $table_name WHERE value LIKE '%$search_text%' LIMIT 10"
+		);
+
+		// MAKE SURE ASSOC ARRAY IS ASSOC ARRAY HERE
+
+		$output_array = array_map(function($x) {
+			return $x->value;
+		}, $results);
+
+		return $output_array;
+	}
 
 }
