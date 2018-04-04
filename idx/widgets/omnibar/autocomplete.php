43a40416d006e91b1051f001ad9d56317313727a
Erik Brown <erikb@IDX.LOCAL>
2018-04-04 16:11:12 -0700

    Now selects distinct in case of duplicate addresses from an MLS

--- idx/widgets/omnibar/autocomplete.php
+++ idx/widgets/omnibar/autocomplete.php
@@ -9,20 +9,25 @@ class Autocomplete {
 
 	public function get_autocomplete_data( $data ) {
 
-		check_ajax_referer('wp_rest', '_wpnonce');
+		check_ajax_referer( 'wp_rest', '_wpnonce' );
 
-		$search_text = urldecode($data['query']);
+		$search_text = urldecode( $data['query'] );
 
 		global $wpdb;
 
 		$table_name = $wpdb->prefix . 'idxbroker_autocomplete_values';
 
 		// Direct db call is fine, since we can't cache every search variant
+		// Selects distinct in case of duplicate address in MLS
 		$results = $wpdb->get_results(
-			"SELECT value FROM $table_name WHERE value LIKE '%$search_text%' LIMIT 10"
+			"SELECT DISTINCT value FROM $table_name WHERE value LIKE '%$search_text%' LIMIT 10"
 		);
 
-		$output_array = array_map(function($x) {
+		if ( ! is_array( $results ) ) {
+			return [];
+		}
+
+		$output_array = array_map( function( $x ) {
 			return $x->value;
 		}, $results);
 
