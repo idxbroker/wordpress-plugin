bbae8cf3b80d32b7218ff2e670d61f7abbd5bc4b
Dave Bonds <dave@idxbroker.com>
2018-04-27 09:23:15 -0700

    actually working. needs cleanup.

--- idx/backward-compatibility/migrate-legacy-widgets.php
+++ idx/backward-compatibility/migrate-legacy-widgets.php
@@ -45,7 +45,9 @@ class Migrate_Legacy_Widgets {
 						$key = ( is_int( $instances ) ) ? $instances : null;
 						if ( null !== $key ) {
 							$active_widgets[] = array(
-								'widget_id'       => $widget_base_id . '-' . $key,
+								'widget_base_id'  => $widget_base_id,
+								'key'             => $key,
+								'widget_uid'      => $widget->uid,
 								'widget_instance' => $instance,
 							);
 						}
@@ -60,80 +62,51 @@ class Migrate_Legacy_Widgets {
 
 	public function convert_mw_widgets( $active_widgets ) {
 		$active_widgets = $this->get_active_mw_widgets();
-
+		//var_dump($active_widgets);die;
 		foreach ( $active_widgets as $active_widget ) {
 			$widgets_converted[] = $this->convert_widget( $active_widget );
 		}
 
-		if ( is_array( $widgets_converted ) ) {
+		if ( $widgets_converted && is_array( $widgets_converted ) ) {
 			// update_option();
 			return true;
 		}
 
-		return;
+		return false;
 	}
-	
-
-	// public function get_and_convert_active_mw_widgets() {
-	// 	$idx_widgets = $this->idx_api->idx_api_get_widgetsrc();
-
-	// 	if ( ! is_array( $idx_widgets ) ) {
-	// 		return;
-	// 	}
-
-	// 	foreach ( $idx_widgets as $widget ) {
-	// 		$widget_base_id = 'idx' . str_replace( '-', '_', $widget->uid );
-	// 		$widget_instances = get_option( 'widget_' . $widget_base_id );
-	// 		$sidebar_widgets = get_option( 'sidebars_widgets' );
-	// 		if ( false !== $widget_instances ) {
-	// 			foreach ( $widget_instances as $instances => $instance ) {
-	// 				// If it's an array, we have a widget here. Convert it.
-	// 				if ( is_array( $instance ) ) {
-	// 					$key = ( is_int( $instances ) ) ? $instances : null;
-	// 					if ( null !== $key ) {
-	// 						$active_widgets[] = $widget->uid . '-' . $key;
-	// 						$this->convert_widget( $widget->uid, $key, $instance, $sidebar_widgets );
-	// 					}
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-
-	// 	return ( $active_widgets ) ? $active_widgets : false;
-	// }
 
 	public function convert_widget( $active_widget ) {
 		if ( ! is_array( $active_widget ) ) {
 			return null;
 		}
-		var_dump($active_widget);
-		die;
+
 		// Get the new widget instances.
 		$new_widget_instance = get_option( 'widget_impress_idx_dashboard_widget' );
 
-		// Get replaceable widgets.
-		$replaceable_widgets = $this->get_replaceable_widgets( $active_widget['widget_instance'] );
-		var_dump($replaceable_widgets);
-		die;
+		// Get sidebar widgets
+		$sidebar_widgets = get_option( 'sidebars_widgets' );
 
 		// Loop through registered sidebars (widget areas).
 		foreach ( $sidebar_widgets as $widget_area_ids => $widgets ) {
-		//foreach ( $sidebar_widgets as $widget_areas ) {
+			//var_dump($widgets); die;
+
 			// Make sure it's an array.
 			if ( ! is_array( $widgets ) ) {
 				return;
 			}
 
+			// Set the current widget area id.
+			$current_widget_area_id = $widget_area_ids;
+
 			// Loop through widget areas and get widget id's (names).
-			foreach ( $widgets as $widget_id ) {
+			foreach ( $widgets as $widget_key => $widget_id ) {
 				// If current widget ID matches the widget_base_id+key, this is the one we need to replace.
-				if ( $widget_base_id . '-' . $key === $widget_id ) {
-					// Set current widget id, which is the $sidebar_widgets array key.
-					$current_widget_area_id = $widget_area_ids;
-					// Build new widget data. Use existing title and match up URL.
+				if ( $active_widget['widget_base_id'] . '-' . $active_widget['key'] === $widget_id ) {
+					// Build new widget data. Use existing title and match up URL using widget_uid.
+					//var_dump($active_widget);die;
 					$new_widget_data = array(
-						'title' => $instance['title'],
-						'url'   => $this->get_widget_url( $widget_uid ),
+						'title' => $active_widget['widget_instance']['title'],
+						'url'   => $this->get_widget_url( $active_widget['widget_uid'] ),
 					);
 
 					// Retrieve the key of the next widget instance.
@@ -143,6 +116,7 @@ class Migrate_Legacy_Widgets {
 					// Merge our new widget data into the new widget instance.
 					$new_widget_instance[$next_key] = $new_widget_data;
 
+					//var_dump($new_widget_instance);die;
 					// Update the widget option with the new widget instance data.
 					// update_option( 'widget_impress_idx_dashboard_widget', $new_widget_instance );
 
@@ -150,63 +124,18 @@ class Migrate_Legacy_Widgets {
 					$new_widget = 'widget_impress_idx_dashboard_widget' . '-' . $next_key;
 
 					// Finally, update the sidebar widget options to maintain the position of the widget in the sidebar array.
-					//var_dump( $sidebar_widgets );
-					$sidebar_widgets[ $current_widget_area_id ][ $widgets ] = $new_widget;
-					echo '<hr /><hr />';
-					//var_dump( $sidebar_widgets );
+					$sidebar_widgets[ $current_widget_area_id ][ $widget_key ] = $new_widget;
+
+					var_dump( $sidebar_widgets );
 					// update_option( 'sidebars_widgets', $sidebar_widgets );
 				}
 			}
-
-			// if ( is_array( $widgets ) ) {
-			// 	// Loop through widget areas and get widget id's.
-			// 	foreach ( $widget_areas as $widget_key => $widget_id ) {
-			// 		var_dump(key($widget_areas));
-			// 		// If current widget ID matches the widget_base_id+key, this is the one we need to replace.
-			// 		if ( $widget_base_id . '-' . $key === $widget_id ) {
-			// 			//$widget_area_id = ?;
-			// 			//var_dump($widget_area_id);
-			// 			// Build new widget data. Use existing title and match up URL.
-			// 			$new_widget_data = array(
-			// 				'title' => $instance['title'],
-			// 				'url'   => $this->get_widget_url( $widget_uid ),
-			// 			);
-
-			// 			// Retrieve the key of the next widget instance.
-			// 			$numeric_keys = array_filter( array_keys( $new_widget_instance ), 'is_int' );
-			// 			$next_key = $numeric_keys ? max( $numeric_keys ) + 1 : 2;
-
-			// 			// Merge our new widget data into the new widget instance.
-			// 			$new_widget_instance[$next_key] = $new_widget_data;
-
-			// 			//var_dump($new_widget_instance);
-
-			// 			// Update the option with the new widget instance data.
-			// 			//update_option( 'widget_impress_idx_dashboard_widget', $new_widget_instance );
-
-			// 			// Set $new_widget with appended key.
-			// 			$new_widget = 'widget_impress_idx_dashboard_widget' . '-' . $next_key;
-			// 			// Update the sidebar widget options to maintain the position of the widget in the sidebar array.
-			// 			//var_dump( $sidebar_widgets );
-			// 			$sidebar_widgets[ $widget_area_id ][ $widget_key ] = $new_widget;
-			// 			echo '<hr /><hr />';
-			// 			//var_dump( $sidebar_widgets );
-			// 			// update_option( 'sidebars_widgets', $sidebar_widgets );
-			// 		}
-			// 	}
-			// }
 		}
-		//}
-	}
-
-	public function get_replaceable_widgets( $widget_instance ) {
-		if ( $widget_instance)
-			var_dump($widget_instance);
 	}
 
 	public function get_widget_url( $widget_uid ) {
 		$idx_widgets = $this->idx_api->idx_api_get_widgetsrc();
-		foreach ( $idx_widgets as $widgets ) {
+		foreach ( $idx_widgets as $widget ) {
 			if ( $widget_uid === $widget->uid ) {
 				return $widget->url;
 			}
