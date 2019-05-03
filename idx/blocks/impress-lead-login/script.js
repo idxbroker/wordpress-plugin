2e341d53c9fee3043b05437c2a5401deb2d8e00e
Andrew Nordahl <drewdahl@gmail.com>
2019-05-02 21:19:24 -0700

    Block options save after update

--- idx/blocks/impress-lead-login/script.js
+++ idx/blocks/impress-lead-login/script.js
@@ -2,12 +2,8 @@
 	var el = wp.element.createElement
 	var ServerSideRender = wp.components.ServerSideRender
 	var registerBlockType = wp.blocks.registerBlockType
-
-	function Stars( { stars } ) {
-		return el( 'div', { key: 'stars' },
-			'★'.repeat( stars ),
-			( ( stars * 2 ) % 2 ) ? '½' : '' );
-	}
+	var InspectorControls = wp.editor.InspectorControls
+	var Checkbox = wp.components.CheckboxControl
 
 	blocks.registerBlockType( 'idx-broker-platinum/impress-lead-login-block', {
 		title: 'IMPress Lead Login',
@@ -27,139 +23,52 @@
 		},
 
 		edit: function( props ) {
-			return el( ServerSideRender, {
-				block: 'idx-broker-platinum/impress-lead-login-block',
-				attributes: props.attributes,
-			} );
+			return [
+				el( ServerSideRender, {
+					block: 'idx-broker-platinum/impress-lead-login-block',
+					attributes: props.attributes,
+				} ),
+
+				el( InspectorControls, {},
+					el( Checkbox, {
+						label: 'Default Styles?',
+						value: props.attributes.styles,
+						checked: (props.attributes.styles > 0 ? true : false) ,
+						onChange: ( value ) => { props.setAttributes( { styles: (value > 0 ? 1 : 0 ) } ); },
+					} )
+				),
+
+				el( InspectorControls, {},
+					el( Checkbox, {
+						label: 'Open in a New Window?',
+						value: props.attributes.new_window,
+						checked: (props.attributes.new_window > 0 ? true : false),
+						onChange: ( value ) => { props.setAttributes( { new_window: (value > 0 ? 1 : 0 ) } ); },
+					} )
+				),
+
+				el( InspectorControls, {},
+					el( Checkbox, {
+						label: 'Add password form field?',
+						value: props.attributes.password_field,
+						checked: (props.attributes.password_field ? true : false),
+						onChange: ( value ) => { props.setAttributes( { password_field: value } ); },
+					} )
+				),
+			]
 		},
 
-		save: function() {
-			// We don't want to save any HTML in post_content, as the value will be in postmeta
-			return null;
+		save: function(props) {
+			return el(
+				null,
+				null,
+				props.attributes.content
+			);
 		}
+
 	} );
 } )(
 	window.wp.blocks,
 	window.wp.element
 );
 
-
-
-
-
-
-
-
-
-
-
-
-// ( function( blocks, element ) {
-// 	var el = element.createElement;
-
-// 	function Stars( { stars } ) {
-// 		return el( 'div', { key: 'stars' },
-// 			'★'.repeat( stars ),
-// 			( ( stars * 2 ) % 2 ) ? '½' : '' );
-// 	}
-
-// 	blocks.registerBlockType( 'idx-broker-platinum/impress-lead-login-block', {
-// 		title: 'IMPress Lead Login',
-// 		icon: 'admin-network',
-// 		category: 'widgets',
-
-// 		attributes: {
-// 			stars: {
-// 				type: 'int',
-// 				meta: 'stars', // Store the value in postmeta
-// 			}
-// 		},
-
-// 		edit: function( props ) {
-// 			var stars = props.attributes.stars,
-// 				children = [];
-
-// 			function setStars( event ) {
-// 				props.setAttributes( { stars: event.target.value } );
-// 				event.preventDefault();
-// 			}
-
-// 			if ( stars ) {
-// 				children.push( Stars( { stars: stars } ) );
-// 			}
-		
-// 			children.push(
-// 				el( 'h5', null, 'IMPress Lead Login' )
-// 			);
-
-// 			children.push(
-// 				el( 'input', {
-// 					key: 'stars-input',
-// 					type: 'number',
-// 					min: 0,
-// 					max: 5,
-// 					step: 0.5,
-// 					value: stars,
-// 					onChange: setStars } )
-// 			);
-
-// 			return el( 'div', { onSubmit: setStars }, children );
-// 		},
-
-// 		save: function() {
-// 			// We don't want to save any HTML in post_content, as the value will be in postmeta
-// 			return null;
-// 		}
-// 	} );
-// } )(
-// 	window.wp.blocks,
-// 	window.wp.element
-// );
-
-
-
-// // wp.blocks.registerBlockType('idx-broker-platinum/impress-lead-login', {
-// // 	title: 'Impress Lead Login',
-// // 	icon: 'megaphone',
-// // 	category: "common",
-// // 	attributes: {
-// // 		content: {
-// // 			type: 'array',
-// // 			source: 'children',
-// // 			selector: 'p',
-// // 		},
-// // 	},
-// // 	edit: function() {
-// // 		var el = element.createElement;
-// // 		return el(
-// // 				'p',
-// // 				{ style: blockStyle },
-// // 				'Hello World, step 1 (from the editor).'
-// // 		);
-// // },
-// // save: function() {
-// // 	var el = element.createElement;
-// // 		return el(
-// // 				'p',
-// // 				{ style: blockStyle },
-// // 				'Hello World, step 1 (from the frontend).'
-// // 		);
-// // },
-
-// 	// edit: function(props) {
-// 	// 	return wp.element.createElement(wp.blocks.RichText, {
-// 	// 		tagName: 'p',
-// 	// 		className: props.className,
-// 	// 		value: props.attributes.content,
-// 	// 		onChange: function(newContent) {
-// 	// 			props.setAttributes({content:newContent});
-// 	// 		}
-// 	// 	});
-// 	// },
-
-// 	// save: function(props) {
-// 	// 	return wp.element.createElement('p', {
-// 	// 		className: props.className,
-// // 	// 	}, props.attributes.content);
-// // 	// }
-// // });
--- idx/blocks/impress-lead-signup/script.js
+++ idx/blocks/impress-lead-signup/script.js
@@ -3,7 +3,7 @@
 	var ServerSideRender = wp.components.ServerSideRender
 	var InspectorControls = wp.editor.InspectorControls
 	var TextControl = wp.components.TextControl
-	var Checkbox = wp.components.Checkbox
+	var Checkbox = wp.components.CheckboxControl
 	var registerBlockType = wp.blocks.registerBlockType
 
 	blocks.registerBlockType( 'idx-broker-platinum/impress-lead-signup-block', {
@@ -28,7 +28,8 @@
 				type: 'bool'
 			},
 			button_text: {
-				type: 'string'
+				type: 'string',
+				default: 'Sign Up!'
 			}
 		},
 
@@ -40,6 +41,42 @@
 				} ),
 
 				el( InspectorControls, {},
+					el( Checkbox, {
+						label: 'Show phone number field?',
+						value: props.attributes.phone,
+						checked: (props.attributes.phone > 0 ? true : false),
+						onChange: ( value ) => { props.setAttributes( { phone: (value > 0 ? 1 : 0 ) } ); },
+					} )
+				),
+
+				el( InspectorControls, {},
+					el( Checkbox, {
+						label: 'Default Styles?',
+						value: props.attributes.styles,
+						checked: (props.attributes.styles > 0 ? true : false) ,
+						onChange: ( value ) => { props.setAttributes( { styles: (value > 0 ? 1 : 0 ) } ); },
+					} )
+				),
+
+				el( InspectorControls, {},
+					el( Checkbox, {
+						label: 'Open in a New Window?',
+						value: props.attributes.new_window,
+						checked: (props.attributes.new_window > 0 ? true : false),
+						onChange: ( value ) => { props.setAttributes( { new_window: (value > 0 ? 1 : 0 ) } ); },
+					} )
+				),
+
+				el( InspectorControls, {},
+					el( Checkbox, {
+						label: 'Add password form field?',
+						value: props.attributes.password_field,
+						checked: (props.attributes.password_field ? true : false),
+						onChange: ( value ) => { props.setAttributes( { password_field: value } ); },
+					} )
+				),
+
+				el( InspectorControls, {},
 					el( TextControl, {
 						label: 'Sign up button text:',
 						value: props.attributes.button_text,
@@ -47,33 +84,17 @@
 					} )
 				),
 
-				// el( InspectorControls, {},
-				// 	el( RadioControl, {
-				// 		label: 'Foo1',
-				// 		value: props.attributes.foo1,
-				// 		onChange: ( value ) => { props.setAttributes( { foo1: value } ); },
-				// 	} )
-				// )
 			]
 		},
 
-		/**
-		 * 
-		 * RadioControl
-		label="User type"
-		help="The type of the current user"
-		selected={ option }
-		options={ [
-			{ label: 'Author', value: 'a' },
-			{ label: 'Editor', value: 'e' },
-		] }
-		onChange={ ( option ) => { setState( { option } ) } }
-		 */
-
-		save: function() {
-			// We don't want to save any HTML in post_content, as the value will be in postmeta
-			return null;
+		save: function(props) {
+			return el(
+				null,
+				null,
+				props.attributes.content
+			);
 		}
+
 	} );
 } )(
 	window.wp.blocks,
