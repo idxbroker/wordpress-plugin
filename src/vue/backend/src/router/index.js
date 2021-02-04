6c2bcb4a28d8d775e437b582c05e872b8abfd237
sgreenl5 <greenlaw_sarah@yahoo.com>
2021-02-04 08:53:23 -0800

    UXG-1280: Added Route structure

--- src/vue/backend/src/router/index.js
+++ src/vue/backend/src/router/index.js
@@ -1,22 +1,170 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
     {
-        path: '/',
-        name: 'Home',
-        component: Home
+        path: '/import/listings',
+        name: 'Import IDX Listings',
+        // component
+        children: [
+            {
+                path: 'imported',
+                name: 'Imported IDX Listings'
+                // component
+            },
+            {
+                path: 'unimported',
+                name: 'Unimported IDX Listings'
+                // component
+            }
+        ]
     },
     {
-        path: '/about',
-        name: 'About',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+        path: '/import/agents',
+        name: 'Import Agents',
+        // component
+        children: [
+            {
+                path: 'imported',
+                name: 'Imported Agents'
+                // component
+            },
+            {
+                path: 'unimported',
+                name: 'Unimported Agents'
+                // component
+            }
+        ]
+    },
+    {
+        path: '/settings',
+        name: 'Settings',
+        children: [
+            {
+                path: 'general',
+                name: 'IMPress General Settings'
+                // component
+            },
+            {
+                path: 'omnibar',
+                name: 'IMPress Omnibar Settings'
+                // component
+            },
+            {
+                path: 'listings',
+                children: [
+                    {
+                        path: '',
+                        name: 'IMPress Listings General Settings'
+                        // component
+                    },
+                    {
+                        path: '',
+                        name: 'IMPress Listings IDX Settings'
+                        // component
+                    },
+                    {
+                        path: '',
+                        name: 'IMPress Listings Advanced Settings'
+                        // component
+                    }
+                ]
+            },
+            {
+                path: 'agents',
+                name: 'IMPress Agents Settings'
+                // component
+            },
+            {
+                path: 'social-pro',
+                name: 'Social Pro Settings'
+                // component
+            }
+        ]
+    },
+    {
+        path: '/guided-setup',
+        name: 'Guided Setup',
+        // component
+        children: [
+            {
+                path: 'connect',
+                children: [
+                    {
+                        path: 'api',
+                        name: 'Connect Account'
+                        // component
+                    },
+                    {
+                        path: 'general',
+                        name: 'General Settings'
+                        // component
+                    },
+                    {
+                        path: 'omnibar',
+                        name: 'Omnibar Settings'
+                        // component
+                    }
+                ]
+            },
+            {
+                path: 'listings',
+                children: [
+                    {
+                        path: 'activate',
+                        name: 'Activate IMPress Listings'
+                        // component
+                    },
+                    {
+                        path: 'general',
+                        name: 'Configure General IMPress Listings'
+                        // component
+                    },
+                    {
+                        path: 'idx',
+                        name: 'Configure IDX IMPress Listings'
+                        // component
+                    },
+                    {
+                        path: 'advanced',
+                        name: 'Configure Advanced IMPress Listings'
+                        // component
+                    }
+                ]
+            },
+            {
+                path: 'agents',
+                children: [
+                    {
+                        path: 'activate',
+                        name: 'Activate IMPress Agents'
+                        // component
+                    },
+                    {
+                        path: 'configure',
+                        name: 'Configure IMPress Agents'
+                        // component
+                    }
+                ]
+            },
+            {
+                path: 'social-pro',
+                children: [
+                    {
+                        path: 'activate',
+                        name: 'Activate Social Pro'
+                        // component
+                    },
+                    {
+                        path: 'general',
+                        name: 'Configure Social Pro'
+                        // component
+                    }
+                ]
+            }
+        ]
     }
 ]
 
