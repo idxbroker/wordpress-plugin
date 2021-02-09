9247ac8eab9fff3ea3809ed421d4743b5e49bbbc
Nic Marson <nic@idxbroker.com>
2021-02-09 08:13:16 -0800

    UXG-1236 add API Key component

--- src/vue/backend/src/views/Home.vue
+++ src/vue/backend/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png">
+        <APIKey/>
         <HelloWorld msg="Welcome to Your Vue.js App"/>
     </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import APIKey from '@/components/APIKey.vue'
 
 export default {
     name: 'Home',
     components: {
-        HelloWorld
+        HelloWorld,
+        APIKey
     }
 }
 </script>
