2b371b378cd783a4fa17c4d75e2d9f4069108893
Nic Marson <nic@idxbroker.com>
2021-02-10 07:15:49 -0800

    UXG-1236 sets up header and nav areas

--- src/vue/backend/src/views/NetworkIssue.vue
+++ src/vue/backend/src/views/NetworkIssue.vue
@@ -2,6 +2,6 @@
     <div>
         <h1>Uh-Oh!</h1>
         <h3>It looks like you're experiencing some network issues, please click the back button and try again.</h3>
-        <router-link :to="{ name: 'event-list' }">Or go back to the home page</router-link>
+        <router-link :to="{ name: 'Settings' }">Or go back to the home page</router-link>
     </div>
 </template>
