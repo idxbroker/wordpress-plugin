99ae58707e6d135d8ade7f923cd18e933fc639c2
sgreenl5 <greenlaw_sarah@yahoo.com>
2021-02-16 15:07:45 -0800

    UXG-1281: Folder skeleton for store

---
+++ src/vue/backend/src/router/import/index.js
@@ -0,0 +1,14 @@
+import actions from './actions'
+import getters from './getters'
+import mutations from './mutations'
+
+const state = {
+}
+
+export default {
+    namespaced: true,
+    actions,
+    getters,
+    mutations,
+    state
+}
