f55cc4e0688fd22b7113d5058a80f21387979113
sgreenl5 <greenlaw_sarah@yahoo.com>
2021-02-16 15:17:48 -0800

    UXG-1281: Fixed wrong file path

--- src/vue/backend/src/store/index.js
+++ src/vue/backend/src/store/index.js
@@ -1,15 +1,14 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-    state: {
-    },
-    mutations: {
-    },
-    actions: {
-    },
-    modules: {
-    }
-})
+import agentSettings from './modules/agentSettings'
+import general from './modules/general'
+import importContent from './modules/importContent'
+import listingsSettings from './modules/listingsSettings'
+import omnibar from './modules/omnibar'
+import socialPro from './modules/socialPro'
+export default {
+   agentSettings,
+   general,
+   importContent,
+   listingsSettings,
+   omnibar,
+   socialPro
+}
