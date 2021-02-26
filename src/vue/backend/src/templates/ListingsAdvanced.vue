847f9a72657fd928934e683c4d401a8913f1a78d
Nic Marson <nic@idxbroker.com>
2021-02-26 09:37:32 -0800

    UXG-1244 applies global form styles

--- src/vue/backend/src/templates/ListingsAdvanced.vue
+++ src/vue/backend/src/templates/ListingsAdvanced.vue
@@ -1,10 +1,10 @@
 <template>
-    <idx-block tag="fieldset" className="fieldset listings-advanced">
+    <idx-block tag="fieldset" className="fieldset listings-advanced form-content">
         <idx-form-group>
-            <idx-block className="control-label"><strong>CSS Settings</strong> Detailed sentence or two describing deregistering IMPress Listing CSS files so that the installed theme’s CSS won’t have specificity issues.</idx-block>
+            <idx-block className="control-label"><idx-block tag="strong" className="control-label-title">CSS Settings</idx-block> Detailed sentence or two describing deregistering IMPress Listing CSS files so that the installed theme’s CSS won’t have specificity issues.</idx-block>
         </idx-form-group>
         <idx-form-group>
-            <idx-block className="control-toggle-slider">
+            <idx-block className="form-content__toggle">
                 {{ mainCssLabel }}
                 <idx-toggle-slider
                     uncheckedState="No"
@@ -16,7 +16,7 @@
             </idx-block>
         </idx-form-group>
         <idx-form-group>
-            <idx-block className="control-toggle-slider">
+            <idx-block className="form-content__toggle">
                 {{ widgetsCssLabel }}
                 <idx-toggle-slider
                     uncheckedState="No"
@@ -29,10 +29,10 @@
         </idx-form-group>
         <hr/>
         <idx-form-group>
-            <idx-block className="control-label"><strong>Form Submissions to IDX Broker</strong> Send all contact form submissions to IDX Broker as a lead.<br> <b>Note:</b> This option only works while using default contact forms.</idx-block>
+            <idx-block className="control-label"><idx-block tag="strong" className="control-label-title">Form Submissions to IDX Broker</idx-block> Send all contact form submissions to IDX Broker as a lead.<br> <b>Note:</b> This option only works while using default contact forms.</idx-block>
         </idx-form-group>
         <idx-form-group>
-            <idx-block className="control-toggle-slider">
+            <idx-block className="form-content__toggle">
                 {{ sendFormLabel }}
                 <idx-toggle-slider
                     uncheckedState="No"
@@ -44,7 +44,7 @@
             </idx-block>
         </idx-form-group>
         <idx-form-group>
-            <idx-block className="control-label"><strong>Default Form Shortcode</strong> Detailed sentence or two describing short code compatibility with Contact Form plugin. If no short code is entered the template uses default contact forms.</idx-block>
+            <idx-block className="control-label"><idx-block tag="strong" className="control-label-title">Default Form Shortcode</idx-block> Detailed sentence or two describing short code compatibility with Contact Form plugin. If no short code is entered the template uses default contact forms.</idx-block>
         </idx-form-group>
         <idx-form-group>
             <idx-form-label customClass="control-label" for="form-shortcode">Form Shortcode</idx-form-label>
@@ -57,7 +57,7 @@
         </idx-form-group>
         <hr/>
         <idx-form-group>
-            <idx-block className="control-label"><strong>Google Maps</strong> Listings can be automatically mapped if a latitude and longitude is provided. A Google Maps API Key is required - <a href="https://developers.google.com/maps/documentation/javascript/get-api-key">click here</a> to register.</idx-block>
+            <idx-block className="control-label"><idx-block tag="strong" className="control-label-title">Google Maps</idx-block> Listings can be automatically mapped if a latitude and longitude is provided. A Google Maps API Key is required - <a href="https://developers.google.com/maps/documentation/javascript/get-api-key">click here</a> to register.</idx-block>
         </idx-form-group>
         <idx-form-group>
             <idx-form-label customClass="control-label" for="google-maps">Google Maps API Key</idx-form-label>
@@ -70,7 +70,7 @@
         </idx-form-group>
         <hr/>
         <idx-form-group>
-            <idx-block className="control-label"><strong>Custom Wrapper</strong> Detailed sentence or two describing how custom wrappers can be used and how to set them up properly.</idx-block>
+            <idx-block className="control-label"><idx-block tag="strong" className="control-label-title">Custom Wrapper</idx-block> Detailed sentence or two describing how custom wrappers can be used and how to set them up properly.</idx-block>
         </idx-form-group>
         <idx-form-group>
             <idx-form-label customClass="control-label" for="wrapper-start-html">Wrapper Start HTML</idx-form-label>
@@ -92,10 +92,10 @@
         </idx-form-group>
         <hr/>
         <idx-form-group>
-            <idx-block className="control-label"><strong>Plugin Uninstallation</strong> Checking this option will delete <b>all</b> plugin data when uninstalling the plugin.</idx-block>
+            <idx-block className="control-label"><idx-block tag="strong" className="control-label-title">Plugin Uninstallation</idx-block> Checking this option will delete <b>all</b> plugin data when uninstalling the plugin.</idx-block>
         </idx-form-group>
         <idx-form-group>
-            <idx-block className="control-toggle-slider">
+            <idx-block className="form-content__toggle">
                 {{ pluginUninstallationLabel }}
                 <idx-toggle-slider
                     uncheckedState="No"
@@ -141,52 +141,17 @@ export default {
 </script>
 <style lang="scss">
 @import '~bootstrap/scss/forms';
-@import '~@idxbrokerllc/idxstrap/dist/styles/components/customSelect.scss';
-@import '~@idxbrokerllc/idxstrap/dist/styles/components/toggleSlider.scss';
-.fieldset {
-    // Global Styles
-    --space-1: 4px;
-    --space-2: 8px;
-    --space-6: 24px;
-    --font-size-label: 16px;
-    --font-size-p: 16px;
-    --line-height-label: 22px;
-    --line-height-p: 28px;
-    font-size: var(--font-size-p);
-    line-height: var(--line-height-p);
-    hr {
-        border: 1px solid $gray-400;
-        margin-bottom: var(--space-6);
-        margin-top: var(--space-6);
-        opacity: 1;
-    }
-    input[type=text] {
-        border: 1px solid $gray-250;
-        color: $gray-875;
-        line-height: 1.5;
-        padding: 0.625rem 1.25rem;
-    }
-    .form-group {
-        margin-bottom: var(--space-6);
-    }
-    .control-label {
-        display: block;
-        font-size: var(--font-size-label);
-        line-height: var(--line-height-label);
-        margin-bottom: var(--space-2);
-        width: auto;
-        strong {
-            display: block;
-            margin-bottom: var(--space-1);
-        }
-    }
-    .control-toggle-slider {
-        align-items: center;
-        background-color: $gray-150;
-        display: flex;
-        justify-content: space-between;
-        margin-top: var(--space-4);
-        padding: var(--space-3) var(--space-5);
-    }
+@import '~@idxbrokerllc/idxstrap/dist/styles/components/toggleSlider';
+@import '~@idxbrokerllc/idxstrap/dist/styles/components/customSelect';
+@import '../styles/formContentStyles.scss';
+.fieldset hr {
+    border: 1px solid $gray-400;
+    margin-bottom: var(--space-6);
+    margin-top: var(--space-6);
+    opacity: 1;
+}
+.control-label-title {
+    display: block;
+    margin-bottom: var(--space-1);
 }
 </style>
