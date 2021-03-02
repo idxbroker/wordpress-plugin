37364756e0803584c03f8e656522475a9e72866c
Nic Marson <nic@idxbroker.com>
2021-03-02 12:12:44 -0800

    UXG-1269 feedback udpates

--- src/vue/backend/src/store/modules/general/actions.js
+++ src/vue/backend/src/store/modules/general/actions.js
@@ -15,56 +15,6 @@ const verifyAPIkey = () => {
 
 const saveOmnibarSettings = () => {
     // To-Do: add actual endpoints
-    // GET /idxbroker/v1/admin/settings/omnibar
-    // return {
-    //     cityListOptions,
-    //     cityListSelected,
-    //     countyListOptions,
-    //     countyListSelected,
-    //     postalCodeListOptions,
-    //     postalCodeSelected,
-    //     defaultPropertyTypeOptions,
-    //     defaultPropertyTypeSelected,
-    //     mlsMembership: [
-    //         {
-    //             label: 'Regional Multiple Listings Service',
-    //             value: 'a001',
-    //             propertyTypes: [
-    //                 { value: 'single-family', label: 'Single Family'},
-    //                 { value: 'commercial', label: 'Commercial'},
-    //                 etc
-    //             ],
-    //             selected: 'single-family'
-    //         },
-    //         etc
-    //     ],
-    //     autofillMLSOptions,
-    //     autofillMLSSelected,
-    //     customFieldsSelected,
-    //     customFieldsOptions,
-    //     customPlaceholder,
-    //     defaultSortOrderSelected
-    // }
-    // POST /idxbroker/v1/admin/settings/omnibar
-    // Include only what needs to be updated
-    // Request Body {
-    //     cityListSelected,
-    //     countyListSelected,
-    //     postalCodeSelected,
-    //     defaultPropertyTypeSelected,
-    //     mlsMembership: [
-    //         {
-    //             value: 'a001',
-    //             selected: 'commercial'
-    //         },
-    //         etc
-    //     ],
-    //     autofillMLSSelected,
-    //     customFieldsSelected,
-    //     customPlaceholder,
-    //     defaultSortOrderSelected
-    // }
-    // return 200 or error message tbd
     return new Promise(resolve => setTimeout(resolve, 1000))
 }
 
