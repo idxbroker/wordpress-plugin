b4d916cd6ffa532cd19eda82f3dc7f497739c274
sgreenl5 <greenlaw_sarah@yahoo.com>
2021-03-03 12:35:47 -0800

    UXG-1298: PR fixups

--- src/vue/backend/src/store/modules/progressStepper/actions.js
+++ src/vue/backend/src/store/modules/progressStepper/actions.js
@@ -4,13 +4,12 @@ const progressStepperUpdate = ({ commit, state }, payload) => {
     // Make a deep copy of the state to maintain name, total, and icon
     // Replace the active step with the new active step
     const updatedSteps = state.guidedSetupSteps.map((step, key) => {
-        const newStep = {
+        return {
             name: step.name,
             icon: step.icon,
             total: step.total,
             active: payload[key]
         }
-        return newStep
     })
     commit(types.SET_ITEM, {
         key: 'guidedSetupSteps',
