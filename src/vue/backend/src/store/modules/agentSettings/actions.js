9bd5ce100df7e498e185161a382839ce3ddf3c38
Nic Marson <nic@idxbroker.com>
2021-03-08 12:56:39 -0800

    UXG-1276 adds agents configure guided setup page

--- src/vue/backend/src/store/modules/agentSettings/actions.js
+++ src/vue/backend/src/store/modules/agentSettings/actions.js
@@ -1,2 +1,8 @@
+const saveConfigureAgentSettings = ({ commit }, payload) => {
+    // To-Do: api call to corresponding endpoint
+    return new Promise(resolve => setTimeout(resolve, 100))
+}
+
 export default {
+    saveConfigureAgentSettings
 }
