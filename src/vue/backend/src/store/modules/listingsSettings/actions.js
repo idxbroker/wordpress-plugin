fb29a343a1d591dad0e75759c5dbf5b982051e14
Lee Ralls <lee@idxbroker.com>
2021-03-10 11:09:57 -0800

    UXG-1315: Removed save advanced listings settings method from actions.

--- src/vue/backend/src/store/modules/listingsSettings/actions.js
+++ src/vue/backend/src/store/modules/listingsSettings/actions.js
@@ -10,14 +10,9 @@ const saveIDXListingsSettings = ({ commit }, payload) => {
     // To-Do: api call to corresponding endpoint
     return new Promise(resolve => setTimeout(resolve, 100))
 }
-const saveAdvancedListingsSettings = ({ commit }, payload) => {
-    // To-Do: api call to corresponding endpoint
-    return new Promise(resolve => setTimeout(resolve, 100))
-}
 
 export default {
     enableListingsPlugin,
     saveGeneralListingsSettings,
-    saveIDXListingsSettings,
-    saveAdvancedListingsSettings
+    saveIDXListingsSettings
 }
