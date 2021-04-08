a5ea7e8295bec0173b65fbf7dac47fb6e941b5b8
Erik Brown <erik@idxbroker.com>
2021-04-08 12:08:43 -0700

    IMPB-1296: Fixes frontend api object sometimes missing

--- idx/views/admin.php
+++ idx/views/admin.php
@@ -1,6 +1,8 @@
 <?php
 namespace IDX\Views;
 
+wp_enqueue_script( 'wp-api' );
+
 if ( ! current_user_can( 'manage_options' ) ) {
 	wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 }
