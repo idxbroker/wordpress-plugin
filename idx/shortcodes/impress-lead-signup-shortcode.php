94f36a210dd9ebabe132df2d7f2b388c5c3cc81a
lainvineyard <lainvineyard@gmail.com>
2022-02-24 21:37:26 -0800

    Rename 'google-recaptcha' wp_enqueue_script to 'idx-google-recaptcha' to avoid a conflict with Contact Form 7
    Contact Form 7 uses wp_enqueue_script( 'google-recaptcha' ); when embedding reCAPTCHA with its forms---since our plugin was using the same name to enqueue our own recaptcha script, having IMPress activated alongside CF7 resulted in CF7 being unable to receive a recaptcha response, resulting in CF7 submissions being rejected as spam.

--- idx/shortcodes/impress-lead-signup-shortcode.php
+++ idx/shortcodes/impress-lead-signup-shortcode.php
@@ -57,7 +57,7 @@ class Impress_Lead_Signup_Shortcode {
 
 		if ( ! empty( get_option( 'idx_recaptcha_enabled' ) ) || ! empty( get_option( 'idx_recaptcha_site_key' ) ) ) {
 			wp_enqueue_script( 'idx-recaptcha' );
-			wp_enqueue_script( 'google-recaptcha' );
+			wp_enqueue_script( 'idx-google-recaptcha' );
 			wp_enqueue_script( 'jquery' );
 		}
 
